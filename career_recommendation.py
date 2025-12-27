e6970f194d467ad00e2fe3126e6e1fe9c5dbd363
Alhawraa123-coder <danaishere123@icloud.com>
2025-12-28 02:16:17 +0400

    Create career_recommendation.py

---
+++ career_recommendation.py
@@ -0,0 +1,9 @@
+grades = [85, 92, 70]
+
+for grade in grades:
+    if grade >= 90:
+        print("Recommended Career: Engineering or IT")
+    elif grade >= 75:
+        print("Recommended Career: Business or Management")
+    else:
+        print("Recommended Career: Technical Support")
